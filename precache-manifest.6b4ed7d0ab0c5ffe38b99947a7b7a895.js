0a11187e9d33b89824cdbae24a931e77bf7fe933
j-gottlieb <gessejottlieb@gmail.com>
2018-12-18 11:12:13 -0500

    Updates

---
+++ precache-manifest.6b4ed7d0ab0c5ffe38b99947a7b7a895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a1bc39042ecf940668",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.24a1bc39.js"
+  },
+  {
+    "revision": "dac41c15016f5ac8eb52",
+    "url": "/tic-tac-toe-react/static/js/main.dac41c15.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/tic-tac-toe-react/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "dac41c15016f5ac8eb52",
+    "url": "/tic-tac-toe-react/static/css/main.89f4cadd.chunk.css"
+  },
+  {
+    "revision": "08279d3f8004a051881deb7aad574272",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
